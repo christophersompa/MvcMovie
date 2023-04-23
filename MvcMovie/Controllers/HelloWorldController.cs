c4351748825d1dd6a45cdf87a129641cf6a86c6f
Christopher <cgtsompa@gmail.com>
2023-04-23 14:30:47 +0100

    Adding View

--- MvcMovie/Controllers/HelloWorldController.cs
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -12,23 +12,34 @@ namespace MvcMovie.Controllers
         */
 
         /*
+         * The controller class is where the code is 
+         * written that handles the incoming browser requests.
+         */
+
+        /*
+         * The controller retrieves data from a data source and 
+         * decides what types of response to send back to the browser.
+         */
+
         public IActionResult Index()
+            // IActionResult defines a contract that represents the result of an action method.
         {
+            // calls the controller's view method.
             return View();
         }
-        */
 
-        // GET: /HelloWorld/
-        public string Index()
-        {
-            return "This is my default action...";
-        }
+
+        //public string Index()
+        //{
+        //    return "This is my default action...";
+        //}
 
         // GET: /HelloWorld/Welcome/
         // Requires using System.Text.Encodings.Web;
-        public string Welcome(string name, int numTimes = 1)
+        public string Welcome(string name, int ID = 1)
         {
-            return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+            //https://localhost:{PORT}/HelloWorld/Welcome/3?name=Rick
         }
     }
 }
